0c88d1b2fac4713ce6913e1d1f0fa2cc4923ed84
HYH <huangyinhaow@outlook.com>
2023-04-28 21:19:26 +0800

    init

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://astro-paper.pages.dev/",
+  author: "HYH!",
+  desc: `HYH's personal blog website.`,
+  title: `HYH's Blog`,
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 5,
+};
+
+export const LOCALE = ["zh-CN"]; // set to [] to use the environment default
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+];
